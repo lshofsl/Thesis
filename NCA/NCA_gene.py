04f5fa0ae6aa859e27bac1fbf72eee5a31cb3614
lshofsl <130612237+lshofsl@users.noreply.github.com>
2026-08-13 11:22:14 +0200

    Update NCA_gene.py

--- NCA/NCA_gene.py
+++ NCA/NCA_gene.py
@@ -281,8 +281,8 @@ class NCA_RAMod(nn.Module):
         self.beta_r = nn.Parameter(torch.tensor(0.26)) # Cubic amplitude saturation strength
         self.beta_i = nn.Parameter(torch.tensor(0.0))  #Shear / detuning
         self.K = nn.Parameter(torch.tensor(-1.5)) # Latent Activator spatial coupling 
-        self.raw_beta_d = nn.Parameter(torch.tensor(-5.2)) # Latent decay rate of d (softplus will be apply)
-        self.raw_kappa = nn.Parameter(torch.tensor(0.8)) # Latent d-field diffusion strength
+        self.raw_beta_d = nn.Parameter(torch.tensor(-2.5)) # Latent decay rate of d (softplus will be apply)
+        self.raw_kappa = nn.Parameter(torch.tensor(0.6)) # Latent d-field diffusion strength
         self.dt = 0.1
 
         # Inputs for slow RA perception
